2d320bb77d0aa88a910c8470334a872c0503fc17
Sergey <Sergeev_sergey99@mail.ru>
2018-11-25 02:26:56 +0300

    add final and code style

--- src/Client.java
+++ src/Client.java
@@ -3,15 +3,16 @@ import java.io.*;
 
 public class Client {
     public static void main(String[] args) {
-        int serverPort = 3247;
+        final int SERVER_PORT = 3247;
 
-        String serverAddress = "127.0.0.1";
+//        String serverAddress = "169.254.90.202";
         try {
-            InetAddress ipAddress = InetAddress.getByName(serverAddress);
+            System.out.println();
+            InetAddress ipAddress = InetAddress.getLocalHost();
 
-            System.out.println(serverAddress + ":" + serverPort);
+            System.out.println(ipAddress + ":" + SERVER_PORT);
 
-            Socket socket = new Socket(serverAddress, serverPort);
+            Socket socket = new Socket(ipAddress, SERVER_PORT);
             System.out.println("Connecting...");
 
             InputStream inputStream = socket.getInputStream();
@@ -28,7 +29,7 @@ public class Client {
             System.out.println();
 
             //noinspection InfiniteLoopStatement
-            while (true){
+            while (true) {
                 line = keyboard.readLine();
                 System.out.println("Sending to server...");
 
@@ -40,6 +41,7 @@ public class Client {
                 System.out.println("Server answer is:");
                 System.out.println(line);
                 System.out.println();
+
             }
         } catch (IOException e) {
             e.printStackTrace();
